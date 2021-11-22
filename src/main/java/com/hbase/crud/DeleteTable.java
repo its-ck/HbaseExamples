d40118e71eb7d8acf190c915caae6f7912acaa32
its-ck <cklsc259410@live.com>
2021-11-23 00:04:55 +0530

    Add files via upload

---
+++ src/main/java/com/hbase/crud/DeleteTable.java
@@ -0,0 +1,38 @@
+package com.hbase.crud;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+
+public class DeleteTable {
+
+    public static void main(String[] args) throws IOException {
+
+        Configuration conf = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(conf);
+
+        try {
+            Admin admin = connection.getAdmin();
+            TableName tableName = TableName.valueOf("census");
+
+            if (admin.tableExists(tableName)) {
+                System.out.print("Table exists, Deleting.. ");
+
+                admin.disableTable(tableName);
+                admin.deleteTable(tableName);
+
+                System.out.println(" Done.");
+            } else {
+                System.out.println("Table does not exist.");
+            }
+        } finally {
+            connection.close();
+        }
+    }
+
+}
